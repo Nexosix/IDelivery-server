1687139027249581065cd847b9dfd5253c25533c
Paweł Drozgowski <pawel_drozgowski@zut.edu.pl>
2022-01-26 14:39:09 +0100

    Added CORS

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require("./routes");
+const cors = require('cors');
 
 const app = express();
 const port = 8000;
 
+app.use(cors())
 app.use(express.json())
 app.use('/', router);
 
